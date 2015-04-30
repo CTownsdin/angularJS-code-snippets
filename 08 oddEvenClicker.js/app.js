5917da2a6ea1ae30351a4154f420bb8cd455cb0c
CTownsdin <CTownsdin@users.noreply.github.com>
2015-04-30 13:25:03 -0700

    Update app.js

--- 08 oddEvenClicker.js/app.js
+++ 08 oddEvenClicker.js/app.js
@@ -1,20 +1,14 @@
-// app.js
+'use strict';
 
 var app = angular.module('app', []);
 
 app.controller('MainController', function($scope) {
-  $scope.val = 1
-  $scope.even = false
+  $scope.val = 1;
+  $scope.even = false;
 
   $scope.inc = function() {
-    $scope.val += 1
-    $scope.even = $scope.val%2==0
-  }
+    $scope.val += 1;
+    $scope.even = $scope.val % 2 == 0;
+  };
 
-})
-
-
-// preferred CONTROLLERS way
-// angular.module('controllers', []).controller('MainController', function($scope) {
-//   $scope.val = 'test789';
-// })
+});
